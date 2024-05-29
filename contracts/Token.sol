5cbb1789accec47b4a5596b6a47b08c4f2b61afa
shenshenR0 <168350921+shenshenR0@users.noreply.github.com>
2024-05-29 18:04:36 +0800

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,16 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Token {
-    mapping(address => uint256) public balances;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    constructor(uint256 initialSupply) {
-        balances[msg.sender] = initialSupply;
+contract Token is ERC20 {
+    string private constant _name = "MyToken";
+    string private constant _symbol = "MTK";
+
+    constructor(uint256 initialSupply) ERC20(_name, _symbol) {
+        _mint(msg.sender, initialSupply);
     }
 
-    function transfer(address to, uint256 amount) public {
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(amount <= balanceOf(msg.sender), "ERC20: transfer amount exceeds balance");
+
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function getRandomValue() public pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty))) % 100;
     }
 }
